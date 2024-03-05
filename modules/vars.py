d930a1456ed4f11106ec0b457480d9c65253309a
harat75 <162279439+harat75@users.noreply.github.com>
2024-03-05 11:35:45 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = environ['20606770'] 
+api_hash = environ['fe5ff028c55f4a9cb1e615b35e520f43'] 
+bot_token = environ['6986805955:AAGKYypXGeMfh-Mu39zvyRWiCgNl2hs_Mrc']
