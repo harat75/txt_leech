e61ae69f0b141943833efd74e67f43c63e8825aa
harat75 <162279439+harat75@users.noreply.github.com>
2024-03-06 14:54:43 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = "21964421"
+api_hash = "8f568d3c24f7499656c319444f5d1207"
+bot_token = "7167565820:AAEh268YXIm7radRICkoN1x5yfcBAIdApL0"
